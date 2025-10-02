29f1bdffaf90b97f44d48366feccccf466b2819e
himanshu singh <himanshusingh7392827928@gmail.com>
2025-10-02 14:11:54 +0530

    Deploy profile page and image

--- one.js
+++ one.js
@@ -1,14 +1,20 @@
-const express=require("express");
+const express = require("express");
 const path = require("path");
-const app=express();
-const port=5000;
-app.get("/",(req,res)=>{
-    res.send("helllo word")
-})
-app.get("/about",(req,res)=>{
-    res.sendFile(path.join(__dirname,"profile.html"))
+const app = express();
+const port = 5000;
 
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/about", (req, res) => {
+    res.send("Himanshu singh");
 });
-app.listen(port,()=>{
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "profile.html"));
+});
+
+app.listen(port, () => {
     console.log(`server is 5000 http://localhost:${port}`);
-}); 
+});
